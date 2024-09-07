40befd2fe8d7c4bd6643891574cbe4a31bde52af
klhaug <kristian@snougle.no>
2024-09-07 15:58:18 +0200

    rnaomd buttons

--- script.js
+++ script.js
@@ -5,4 +5,36 @@ button.addEventListener("click", () => {
 })
 
 
+const promise = new Promise((resolve, reject) => {
+    if(true) {
+    resolve ("Stuff worked");
+    } else {
+    reject ("Error, it broke")
+    }
+})
 
+const promise2 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 100, 'Hiii!')
+})
+
+const promise3 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 1000, 'POOKE!')
+})
+
+const promise4 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 5000, 'Is it me your looking for?')
+})
+
+Promise.all([promise, promise2, promise3, promise4])
+    .then(values => {
+        console.log(values)
+    })
+
+
+promise
+    .then(result => result + "!")
+    .then(result2 => {
+        throw Error
+        console.log(result2);
+    })
+    .catch(console.log)
