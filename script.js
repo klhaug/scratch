723a6623daec236b32a71a00da0663884a92e77c
klhaug <kristian@snougle.no>
2024-09-09 07:10:39 +0200

    learning async await

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ const promise = new Promise((resolve, reject) => {
     }
 })
 
+
+
 const promise2 = new Promise((resolve, reject) => {
     setTimeout(resolve, 100, 'Hiii!')
 })
@@ -53,5 +55,25 @@ promise
         console.log(result[1]),
         console.log(result[2])
     }).catch(() => console.log("ERROR"))
-    
 
+
+
+async function fetchUsers() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users")
+    const data = await response.json();
+    console.log(data)
+}
+
+
+const getData = async function() {
+    try {
+        const [ users, posts, albums ] = await  Promise.all(urls.map(url => {
+            return fetch(url).then(resp=> resp.json())
+        }))
+            console.log("users", users),
+            console.log("posts", posts),
+            console.log("albums", albums)
+    } catch {
+        console.log("oopsidoopsi")
+    }
+}
